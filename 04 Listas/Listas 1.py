7c7825bb05b329b6efab70b6b3c99699483cc4bc
valedelluu-byte <valedelluu@gmail.com>
2026-08-28 13:32:43 -0300

    Add files via upload

---
+++ 04 Listas/Listas 1.py
@@ -0,0 +1,58 @@
+ # Actividad 1: Crear una lista con los números del 1 al 100 que sean múltiplos de 4. Utilizar la función range.
+multiplos_de_4 = list(range(4, 101, 4))
+print(multiplos_de_4)
+# Actividad 2. Crear una lista con 5 cosas que te gusten y luego imprimir el penultimo elemento de esa lista.
+cosas_que_me_gustan = ["leer", "comer", "dormir", "jugar", "viajar"]
+print(cosas_que_me_gustan[-2])
+# Actividad 3: Crear una lista vacía, agregar tres palabras con append e imprimir la lista resultante por antalla.
+lista_vacia = []
+lista_vacia.append("manzana")
+lista_vacia.append("banana")
+lista_vacia.append("cereza")
+print(lista_vacia)
+# Actividad 4: Reemplazar el segundo y último valor de la lista “animales” con las palabras “loro” y “oso”,respectivamente. Imprimir la lista resultante por pantalla.
+animales = ["perro", "gato", "pez", "conejo"]
+animales[1] = "loro"
+animales[-1] = "oso"
+print(animales)
+# Actividad 5:Analizar el siguiente programa y explicar con tus palabras qué es lo que realiza
+# Programa:
+numeros = [8, 15, 3, 22, 7]
+numeros.remove(max(numeros))
+print(numeros)
+# Explicacion: Primero tenemos la variable "numeros" que es una lista con 5 numeros. Debajo tenemos la fincion max(numeros), que lo que hace es buscar y devolver el valor mayor de la lista asignada anteriormente, que en este caso seria el "22".Este valor pasa por la funcion .remove(),esta funcion busca t remueve de la lista el elemento especifico. Finalmente, mediante la instrucción print(numeros), se muestra la lista resultante sin su valor máximo, imprimiendo en pantalla [8, 15, 3, 7].
+# Actividad 6: Crear una lista con números del 10 al 30 (incluído), haciendo saltos de 5 en 5 y mostrar por pantalla los dos primeros.
+numeros_del_10_al_30 = list(range(10, 31, 5))
+print(numeros_del_10_al_30[:2])
+# Actividad 7: Remplazar los 2 valores centrales (indice 1 y 2) de la lista "autos" por dos nuevos valores cualquiera.
+autos = ["sedan", "polo", "suran", "gol"]
+autos[1:3] = ["camioneta", "hatchback"]
+print(autos)
+# Actividad 8: Crear una lista vacía llamada "dobles" y agregar el doble de 5, 10 y 15 usando append directamente. Imprimir la lista resultante por pantalla.
+dobles = []
+dobles.append(5 * 2)
+dobles.append(10 * 2)
+dobles.append(15 * 2)
+print(dobles)
+# Actividad 9: Dada la lista “compras”, cuyos elementos representan los productos comprados por diferentes clientes:
+compras = [["pan", "leche"], ["arroz", "fideos", "salsa"],["agua"]]
+# a) Agregar "jugo" a la lista del tercer cliente usando append.
+# b) Reemplazar "fideos" por "tallarines" en la lista del segundo cliente
+# c) Eliminar "pan" de la lista del primer cliente.
+# d) Imprimir la lista resultante por pantalla.
+compras[2].append("jugo")
+compras[1][1] = "tallarines"
+compras[0].remove("pan")
+print(compras)
+# Actividad 10: Elaborar una lista anidada llamada “lista_anidada” que contenga los siguientes elementos:
+# Posición lista_anidada[0]: 15
+# Posición lista_anidada[1]: True
+# Posición lista_anidada[2][0]: 25.5
+# Posición lista_anidada[2][1]: 57.9
+# Posición lista_anidada[2][2]: 30.6
+# Posición lista_anidada[3]: False
+# Imprimir la lista resultante por pantalla
+lista_anidada = [15, True, [25.5, 57.9, 30.6], False]
+print(lista_anidada)
+
+                        
